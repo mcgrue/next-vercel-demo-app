4f9b56c0a7349a757ed63f9f66fed8e4b8773351
Ben McGraw <ben.mcgraw@gmail.com>
2022-05-18 12:47:21 -0700

    First post

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import Date from '../components/date';
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+ 
+
+export default function Home({ allPostsData }) {
+	
+	return (
+	  <Layout home>
+		{/* Keep the existing code here */}
+  
+		{/* Add this <section> tag below the existing <section> tag */}
+		<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+		  <h2 className={utilStyles.headingLg}>Blog</h2>
+		  <ul className={utilStyles.list}>
+			{allPostsData.map(({ id, date, title }) => (
+				MiniPost(id, title, date)
+			))}
+		  </ul>
+		</section>
+	  </Layout>
+	);
+
+	function MiniPost(id, title, date) {
+		return <li className={utilStyles.listItem} key={id}>
+			<Link href={`/posts/${id}`}>
+				<a>{title}</a>
+			</Link>
+			<br />
+			<small className={utilStyles.lightText}>
+				<Date dateString={date} />
+			</small>
+		</li>;
+	}
+  }
+  
